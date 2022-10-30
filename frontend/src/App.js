2052c90f670c4685717a6c0488b84145ea96c9e8
kheder yazgi <108769852+khederyazgi@users.noreply.github.com>
2022-10-30 19:09:21 +0100

    update 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,39 +18,39 @@ function App() {
   return (
 
 
-    <Provider store={store}>
-     <Grid container  sx={{ backgroundColor:"#FFF6F2" }} >
+    // <Provider store={store}>
+    //  <Grid container  sx={{ backgroundColor:"#FFF6F2" }} >
 
 
-                  <Grid item lg={12} >
-                     <Header/> 
+    //               <Grid item lg={12} >
+    //                  <Header/> 
 
-                </Grid>
+    //             </Grid>
 
 
-                <Grid item lg={12}>
-                    <Sectionheader></Sectionheader>
-                </Grid>
-                <Grid item lg={12}>
-                    <Nav style={{backgroundColor:"#FFCCB4",height :"45px"}} /> 
-                </Grid>   
-                <Grid item lg={12} sx={{ height:"44px" }} />
+    //             <Grid item lg={12}>
+    //                 <Sectionheader></Sectionheader>
+    //             </Grid>
+    //             <Grid item lg={12}>
+    //                 <Nav style={{backgroundColor:"#FFCCB4",height :"45px"}} /> 
+    //             </Grid>   
+    //             <Grid item lg={12} sx={{ height:"44px" }} />
 
-                <Grid item lg={12}>
-                    <Filter/>
+    //             <Grid item lg={12}>
+    //                 <Filter/>
 
                     
-                </Grid>   
-                 <Grid item lg={12} sx={{ height:"50px" }} />
-                {/* <Grid item lg={12}  sx={{ backgroundImage: `url(${Image})`, backgroundPosition: "bottom  right", backgroundSize: "581.06px 430.3658447265625px",backgroundRepeat: "no-repeat"}}>
-                     <StickyHeadTable/>
+    //             </Grid>   
+    //              <Grid item lg={12} sx={{ height:"50px" }} />
+    //             {/* <Grid item lg={12}  sx={{ backgroundImage: `url(${Image})`, backgroundPosition: "bottom  right", backgroundSize: "581.06px 430.3658447265625px",backgroundRepeat: "no-repeat"}}>
+    //                  <StickyHeadTable/>
                      
-                </Grid>   */}
-      </Grid>
-      </Provider>
-    // <div>
-    // <Home/>
-    // </div>
+    //             </Grid>   */}
+    //   </Grid>
+    //   </Provider>
+    <div>
+    <Home/>
+    </div>
 
 
  
